8ec881cae667e4ee7d5658ef94aaed15bfdbb054
Albert <albertarko3@gmail.com>
2025-12-23 14:46:50 +0000

    setting up directory structure

---
+++ infrastructure/modules/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "tf_backend" {
+  bucket = "my-terraform-backend-albert"
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
