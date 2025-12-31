412697889da92516dc83c609ad618a845cabdcd7
Albert <albertarko3@gmail.com>
2025-12-31 15:23:25 +0000

    initial rds config

--- infrastructure/modules/rds/locals.tf
+++ infrastructure/modules/rds/locals.tf
@@ -1,10 +1,12 @@
 locals {
   # Determine database port based on engine type
-  db_port = var.engine == "postgres" ? 5432 : 
-            var.engine == "mysql" ? 3306 : 
-            var.engine == "oracle" ? 1521 : 
-            var.engine == "sqlserver" ? 1433 : 
-            var.engine == "mariadb" ? 3306 : 5432  # default to PostgreSQL
+  db_port = {
+    "postgres"  = 5432
+    "mysql"     = 3306
+    "oracle"    = 1521
+    "sqlserver" = 1433
+    "mariadb"   = 3306
+  }[var.engine]
   
   # Create consistent resource names
   resource_prefix = "${var.name_prefix}-${var.environment}"
@@ -14,8 +16,4 @@ locals {
   
   # Set backup retention based on environment
   backup_retention = var.environment == "prod" ? 30 : var.backup_retention_period
-  
-  # Parameter group family (for future use if add parameter groups are added)
-  parameter_group_family = var.engine == "postgres" ? "postgres${split(".", var.engine_version)[0]}${split(".", var.engine_version)[1]}" : 
-                           var.engine == "mysql" ? "mysql${split(".", var.engine_version)[0]}${split(".", var.engine_version)[1]}" : null
 }
