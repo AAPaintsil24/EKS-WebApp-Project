cee8172638a009267b5bd9c3491bb911aa04417c
Albert <albertarko3@gmail.com>
2026-01-02 20:11:20 +0000

    eks modified privatek8s id

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   vpc_cidr                  = var.vpc_cidr
   availability_zones        = var.availability_zones
   public_subnets_cidrs      = var.public_subnets_cidrs
-  private_k8s_subnets_cidrs = var.private_k8s_subnets_cidrs
+  #private_k8s_subnets_cidrs = var.private_k8s_subnets_cidrs
   private_db_subnets_cidrs  = var.private_db_subnets_cidrs
   name_prefix               = var.name_prefix
 }
