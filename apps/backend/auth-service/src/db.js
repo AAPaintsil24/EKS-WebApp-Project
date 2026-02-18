d0671cd6001a7ee62b98aed2cfaf2a99c6876b06
Albert <albertarko3@gmail.com>
2026-02-18 17:44:23 +0000

    Workflows Backend CI/CD

--- apps/backend/auth-service/src/db.js
+++ apps/backend/auth-service/src/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,     // placeholder
   port: process.env.DB_PORT,     // placeholder
   user: process.env.DB_USER,     // placeholder
-  password: process.env.DB_PASS, // placeholder
+  password: process.env.DB_PASSWORD, // placeholder
   database: process.env.DB_NAME  // placeholder
 });
 
